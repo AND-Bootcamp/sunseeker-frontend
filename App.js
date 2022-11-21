5055bfde8afe6e13b47fec0f4154d1d48f085d27
Johan Spekman <johanspekman@QLXK66RW62.home>
2022-11-21 15:24:17 +0100

    [BSS-52] +: Implement styleguide

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GoogleMap from './components/google-map/GoogleMap';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'Roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <GoogleMap />
       <StatusBar style="auto" />
     </View>
