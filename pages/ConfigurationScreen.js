04c9398d0ad6bd849cbe34089ca6008ac7d5c52a
Paolo Brunasti AND <paolo.brunasti@and.digital>
2022-11-22 17:49:15 +0100

    Add logs to each .js page

--- pages/ConfigurationScreen.js
+++ pages/ConfigurationScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const ConfigurationScreen = () => {
+  console.log("ConfiguratioScreen 01");
+
     return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Config!</Text>
